e1912a58ca6aafaf37af87b9ecf72a9e4dc84683
IsaacPushButton <isaac.hurley@gmail.com>
2019-12-15 21:47:44 +1100

    added log to start of history pull calls

--- application.py
+++ application.py
@@ -4,21 +4,27 @@ import time
 import datetime
 import logging
 
-logging.basicConfig( format='%(asctime)s %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
-#minutes
+# minutes
 pull_interval = 5
 recheck_interval = 10
 
 
-
 def pull_recent_history():
+    logging.info("----------------------------------------------------------------")
+    logging.info("Pulling history for all devices since last reading")
+    logging.info("----------------------------------------------------------------")
     historypull.pull_history()
     logging.info("----------------------------------------------------------------")
     logging.info("Recent readings pulled. Next pull in {} minutes ".format(pull_interval))
     logging.info("----------------------------------------------------------------")
 
+
 def recheck_period(hours):
+    logging.info("----------------------------------------------------------------")
+    logging.info("Rechecking data for the last {} hours".format(hours))
+    logging.info("----------------------------------------------------------------")
     historypull.pull_history(hours)
     logging.info("----------------------------------------------------------------")
     logging.info("Recheck complete. Next recheck in {} minutes ".format(pull_interval))
